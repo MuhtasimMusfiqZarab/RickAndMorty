a3a5ded4910d73cc5bfc5cc50d4578d82ea24209
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-07-05 08:32:41 +0600

    navigate from one main file

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,18 +1,48 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import EpisodesScreen from './episodes';
+import EpisodeScreen from './episode';
 import CharactersScreen from './characters';
+import CharacterScreen from './character';
 import LocationsScreen from './locations';
+import LocationScreen from './location';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const EpisodesStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Episodes" component={EpisodesScreen} />
+      <Stack.Screen name="Episode" component={EpisodeScreen} />
+    </Stack.Navigator>
+  );
+};
+const CharactersStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Characters" component={CharactersScreen} />
+      <Stack.Screen name="Character" component={CharacterScreen} />
+    </Stack.Navigator>
+  );
+};
+const LocationsStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Locations" component={LocationsScreen} />
+      <Stack.Screen name="Location" component={LocationScreen} />
+    </Stack.Navigator>
+  );
+};
 
 export default function IndexScreen() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Episodes" component={EpisodesScreen} />
-      <Tab.Screen name="Characters" component={CharactersScreen} />
-      <Tab.Screen name="Locations" component={LocationsScreen} />
+      <Tab.Screen name="Episodes" component={EpisodesStack} />
+      <Tab.Screen name="Characters" component={CharactersStack} />
+      <Tab.Screen name="Locations" component={LocationsStack} />
     </Tab.Navigator>
   );
 }
---
+++ src/screens/location/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+interface Props {}
+function index({}: Props) {
+  return (
+    <View style={styles.container}>
+      <Text>Location Detail</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default index;
